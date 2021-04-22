cd1aac25ea840dd89fd4b5bbb3c724a144f749ed
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-04-22 10:37:10 -0300

    Fix first render without the correct layout

--- src/VisTimeline/VisTimeline.js
+++ src/VisTimeline/VisTimeline.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Timeline from 'react-vis-timeline-2'
 
 export const VisTimeline = ({ data }) => {
+  const timelineRef = useRef(null);
+
   const selectHandler = (props) => {
     console.log("selected");
     console.log(props);
   }
+
+  const tenYearsInMS = 252455616000;
 
   const options = {
     stack: true,
@@ -21,16 +25,24 @@ export const VisTimeline = ({ data }) => {
     horizontalScroll: true,
     type: "range",
     zoomable: false,
-    zoomMin: 252455616000, // 10 years in MS
-    zoomMax: 252455616000,
+    zoomMin: tenYearsInMS,
+    zoomMax: tenYearsInMS,
     showCurrentTime: false,
     editable: false,
     startDate: new Date(2010, 11, 15),
     endDate: new Date(2021, 11, 15)
   };
 
+  useEffect(() => {
+    if (!timelineRef.current) return;
+
+    timelineRef.current.timeline.redraw();
+
+  }, [timelineRef])
+
   return (
     <Timeline
+      ref={timelineRef}
       selectHandler={selectHandler}
       options={options}
       initialItems={data?.items || []}
